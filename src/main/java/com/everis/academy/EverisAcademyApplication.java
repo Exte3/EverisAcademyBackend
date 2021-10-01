b2b9d535ecca2805709f5c3c4bec80e780442419
Gabriel Fuentes <nunkmeconecto@gmail.com>
2021-10-01 13:55:03 -0300

    Commit inicial

---
+++ src/main/java/com/everis/academy/EverisAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.everis.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverisAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverisAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everis/academy/EverisAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everis.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverisAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
