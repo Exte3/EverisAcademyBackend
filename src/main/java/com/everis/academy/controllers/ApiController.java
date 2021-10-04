dc5c3a967924b0f648ac9707ed90f25a446d08d5
cristian rojas <crrojasserrano@gmail.com>
2021-10-04 15:37:25 -0300

    funcionalidades api controller

--- src/main/java/com/everis/academy/controllers/ApiController.java
+++ src/main/java/com/everis/academy/controllers/ApiController.java
@@ -3,21 +3,49 @@ package com.everis.academy.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.everis.academy.models.Profesor;
 import com.everis.academy.services.ProfesorService;
+
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 	
 	@Autowired
 	private ProfesorService profesorService;
+	//Api lista todos los profesores api/profesores
+	@GetMapping("/profesores")
+	public List<Profesor> obtenerProfesores(){
+		return profesorService.findAll();
+	}
+	//Api buscar profesor por su id api/profesores/{id}
+	@GetMapping("/profesores/{id}")
+	public Profesor obtenerProfesor(@PathVariable("id") Long id) {
+		Profesor profesor = profesorService.buscarUsuario(id);
+		if(profesor == null) {
+			throw new RuntimeException("Producto no encontrado"+ id);
+		}
+		return profesor;
+	}
 	
-
+	@CrossOrigin(origins = "http://localhost:4200") //server de Angular Front
+	@PostMapping("agregar_profesor")
+	public Profesor insertarProducto(@Validated @RequestBody Profesor profesor) { //recibe dato
+		System.out.println("intentando insertar producto desde angular");
+		//registry.addMapping("/nuevo_producto").allowedOrigins("http://localhost:4200");
 	
+		profesor.setId(0L);
+		profesorService.insertarProfesor(profesor);
+	    return profesor;
+	}
 	
 	
 	
--- src/main/java/com/everis/academy/services/ProfesorService.java
+++ src/main/java/com/everis/academy/services/ProfesorService.java
@@ -1,12 +1,46 @@
 package com.everis.academy.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.everis.academy.models.Profesor;
 import com.everis.academy.repositories.ProfesorRepository;
+
+
+
 
 @Service
 public class ProfesorService {
 	@Autowired
 	private ProfesorRepository profesorRepository;
+	
+	
+	public List<Profesor> findAll() {
+		// retorna una lista de usuarios
+		return profesorRepository.findAll();
+	}
+	
+	
+	public Profesor buscarUsuario(Long id) {
+
+		Optional<Profesor> oUsuario= profesorRepository.findById(id);
+		
+		if(oUsuario.isPresent()) {
+			return oUsuario.get();
+		}
+		
+		return null;
+	}
+	
+	
+	public Profesor insertarProfesor(@Valid Profesor profesor) {
+		// TODO Auto-generated method stub
+		return profesorRepository.save(profesor);
+	}
+	
 }
