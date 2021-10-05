f2b50ee81dd66732686fb5a1bb62bd4b3bd1c97e
cristian rojas <crrojasserrano@gmail.com>
2021-10-04 22:59:47 -0300

    service controlador

--- src/main/java/com/everis/academy/controllers/ApiController.java
+++ src/main/java/com/everis/academy/controllers/ApiController.java
@@ -21,12 +21,12 @@ public class ApiController {
 	
 	@Autowired
 	private ProfesorService profesorService;
-	//Api lista todos los profesores api/profesores
+	//Api lista todos los profesores http://localhost:8080/api/profesores
 	@GetMapping("/profesores")
 	public List<Profesor> obtenerProfesores(){
 		return profesorService.findAll();
 	}
-	//Api buscar profesor por su id api/profesores/{id}
+	//Api buscar profesor por su id http://localhost:8080/api/profesores/{id}
 	@GetMapping("/profesores/{id}")
 	public Profesor obtenerProfesor(@PathVariable("id") Long id) {
 		Profesor profesor = profesorService.buscarProfesor(id);
@@ -35,7 +35,7 @@ public class ApiController {
 		}
 		return profesor;
 	}
-	
+	//Api agregar profesor  http://localhost:8080/api/agregar_profesor
 	@CrossOrigin(origins = "http://localhost:4200") //server de Angular Front
 	@PostMapping("agregar_profesor")
 	public Profesor insertarProducto(@Validated @RequestBody Profesor profesor) { //recibe dato
@@ -46,6 +46,8 @@ public class ApiController {
 		profesorService.insertarProfesor(profesor);
 	    return profesor;
 	}
+	
+	//Api modificar profesor api/
 	
 	
 	
